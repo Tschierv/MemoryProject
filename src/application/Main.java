1a45fe85d6a27a7f85f22b2db17eac1d17bfe0e9
Tschierv <72274901+Tschierv@users.noreply.github.com>
2020-11-21 17:00:01 +0100

    FXMLLoader

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,26 @@
 package application;
 	
+import java.net.URL;
+import java.nio.file.Paths;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			//	BorderPane root = new BorderPane();
+//Parent root = FXMLLoader.load(getClass().getResource("/src/main/java/com/github/tschierv/memorygame/presentation/MainView.fxml"));
+		//	Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("/src/main/java/com/github/tschierv/memorygame/presentation/MainView.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("./MainView.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
