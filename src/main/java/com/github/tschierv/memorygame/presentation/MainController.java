8b15125abc2245a67c662dc603eeb07e1c2c82c8
Tschierv <72274901+Tschierv@users.noreply.github.com>
2020-11-21 15:30:32 +0100

    Master
    Master

---
+++ src/main/java/com/github/tschierv/memorygame/presentation/MainController.java
@@ -0,0 +1,15 @@
+package main.java.com.github.tschierv.memorygame.presentation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+
+    @FXML
+    private Button MainStart;
+
+
+    @FXML
+    private Button MainExit;
+
+}
