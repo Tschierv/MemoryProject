785f3df49a3dbd8e2bd43a5c5840bc0d4b098d8a
Tschierv <72274901+Tschierv@users.noreply.github.com>
2020-12-06 13:00:49 +0100

    Gui Anpassungen

--- src/main/java/com/github/tschierv/memorygame/presentation/LevelController6x6.java
+++ src/main/java/com/github/tschierv/memorygame/presentation/LevelController6x6.java
@@ -1,5 +1,22 @@
 package main.java.com.github.tschierv.memorygame.presentation;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.text.Text;
+
 public class LevelController6x6 {
+
+    @FXML
+    private ColumnConstraints Grid6x6Column;
+
+    @FXML
+    private RowConstraints Grid6x6Row;
+
+    @FXML
+    private Text Username;
+
+    @FXML
+    private Text Counter;
 
 }
