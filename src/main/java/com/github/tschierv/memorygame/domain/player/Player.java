106f12316d299e0827347b2a29d66f1701fe1466
holec <nerd.pingu@gmail.com>
2021-01-17 17:09:15 +0100

    Improvement: Cleanup of unused resources

--- src/main/java/com/github/tschierv/memorygame/domain/player/Player.java
+++ src/main/java/com/github/tschierv/memorygame/domain/player/Player.java
@@ -6,59 +6,41 @@ public class Player {
 
     public String accountName;
     private UUID accountId;
-    public UUID accountAvatar;
     public Integer score = 0;
-    public Integer playedGames = 0;
 
-    public Player(String AccountName, UUID AccountAvatar) {
+    public Player(String AccountName) {
         this.accountName = AccountName;
         this.accountId = UUID.randomUUID();
-        this.accountAvatar = AccountAvatar;
     }
     public Player()
     {
         super();
     }
-    public Player(String AccountName, UUID AccountId, UUID AccountAvatar) {
+    public Player(String AccountName, UUID AccountId) {
         this.accountName = AccountName;
         this.accountId = AccountId;
-        this.accountAvatar = AccountAvatar;
     }
 
-    public Player(String AccountName, UUID AccountId, UUID AccountAvatar, Integer Score, Integer PlayedGames) {
+    public Player(String AccountName, UUID AccountId, Integer Score) {
         this.accountName = AccountName;
         this.accountId = AccountId;
-        this.accountAvatar = AccountAvatar;
         this.score = Score;
-        this.playedGames = PlayedGames;
     }
 
     public String getAccountName (){
         return this.accountName;
     }
-    public  UUID getAccountId () {
-        return this.accountId;
-    }
-    public UUID getAvatar () {
-        return this.accountAvatar;
-    }
+
     public Integer getScore() {
         return this.score;
-    }
-
-    public Integer getPlayedGames() {
-        return this.playedGames;
     }
 
     public void setScore(Integer Score) {
         this.score = Score;
     }
 
-    public void setPlayedGames(Integer playedGames) {
-        this.playedGames = playedGames;
+    public UUID getAccountId(){
+        return this.accountId;
     }
 
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
 }
